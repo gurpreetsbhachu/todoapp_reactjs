55aacb0403978fda8bdfddfc50718b101b7b5f3a
Gurpreet Singh Bhachu <gurpreetbhachu@github.com>
2020-07-22 19:31:19 -0400

    Updates

---
+++ precache-manifest.cfc6d34af7124f259ec30ed44e2c2c38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54e8400cd21c39e2f2ac6a149508b6d5",
+    "url": "/gurpreetsbhachu/todoapp_reactjs/index.html"
+  },
+  {
+    "revision": "fb01ee06d613f25ed69c",
+    "url": "/gurpreetsbhachu/todoapp_reactjs/static/css/main.34cd4a75.chunk.css"
+  },
+  {
+    "revision": "f8538519eedb7a64242e",
+    "url": "/gurpreetsbhachu/todoapp_reactjs/static/js/2.ad5dea97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gurpreetsbhachu/todoapp_reactjs/static/js/2.ad5dea97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb01ee06d613f25ed69c",
+    "url": "/gurpreetsbhachu/todoapp_reactjs/static/js/main.215f6631.chunk.js"
+  },
+  {
+    "revision": "f2dd412887125088e7ec",
+    "url": "/gurpreetsbhachu/todoapp_reactjs/static/js/runtime-main.9d34da5d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gurpreetsbhachu/todoapp_reactjs/static/media/logo.5d5d9eef.svg"
+  }
+]);
